2e491b6fd9d48260cb79949fae843d7be3cdf191
TIUSHAR BARI <tusharbari1998@gmail.com>
2025-09-16 12:38:19 +0530

    Update 09_formatt_letter.py

--- Chapter_3/09_formatt_letter.py
+++ Chapter_3/09_formatt_letter.py
@@ -1,5 +1,5 @@
 letter = "Dear Tushar, This Python course is nice! Thanks!"
 print(letter)
 
-formatted_letter = "Dear Tushar,\n\tThis Python course is nice!\nThanks!"
+formatted_letter = "Dear Tushar,\n\tThis Python course is nice! \nThanks!"
 print(formatted_letter)
